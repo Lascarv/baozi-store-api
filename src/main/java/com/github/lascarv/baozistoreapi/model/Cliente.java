08e9a767e0b6d8aeeb8410b29ae1e7139fe804c6
Lascarv <lais.desantana05@gmail.com>
2026-08-24 15:39:32 -0300

    criando atributos na classe cliente

--- src/main/java/com/github/lascarv/baozistoreapi/model/Cliente.java
+++ src/main/java/com/github/lascarv/baozistoreapi/model/Cliente.java
@@ -1,5 +1,83 @@
 package com.github.lascarv.baozistoreapi.model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private LocalDate clienteDesde;
+	
+	public Cliente(Long id, String name, LocalDate clienteDesde) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.clienteDesde = clienteDesde;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getClienteDesde() {
+		return clienteDesde;
+	}
+
+	public void setClienteDesde(LocalDate clienteDesde) {
+		this.clienteDesde = clienteDesde;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(clienteDesde, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(clienteDesde, other.clienteDesde) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", name=" + name + ", clienteDesde=" + clienteDesde + "]";
+	}
+	
+	
 
 }
